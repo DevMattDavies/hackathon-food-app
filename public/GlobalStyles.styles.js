5e88a034894e539fb9cc47eea1a2baf4b5c50bcd
Matt Davies <matt@mattdavies.dev>
2022-09-09 09:51:01 +0100

    Added base global style setup

---
+++ public/GlobalStyles.styles.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+:root {
+
+}
+
+    html {
+      font-size: calc(60% + 0.8vmin);
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+    }
+
+    * {
+        font-size: 1rem;
+        box-sizing: border-box;
+    }
+`;
