c9f6e19b849ad5a72b909e57fe37e156b192d5f2
Matt Davies <matt@mattdavies.dev>
2022-09-09 10:53:36 +0100

    Added local menu items data

---
+++ data/menu-data.js
@@ -0,0 +1,34 @@
+export const menuItems = {
+  goatsCheese: {
+    id: 644953,
+    title: "Goat Cheese Pesto Pizza",
+  },
+  BLT: {
+    id: 680975,
+    title: "BLT Pizza",
+  },
+  plantain: {
+    id: 716300,
+    title: "Plantain Pizza",
+  },
+  paneer: {
+    id: 654523,
+    title: "Paneer & Fig Pizza",
+  },
+  butternut: {
+    id: 636593,
+    title: "Butternut Squash Pizza",
+  },
+  shrimp: {
+    id: 1697557,
+    title: "Shrimp Pizza with Pecan Basil Pesto",
+  },
+  peaches: {
+    id: 658920,
+    title: "Rustic Grilled Peaches Pizza",
+  },
+  speck: {
+    id: 637434,
+    title: "Chanterelle and Mangalitsa Speck Pizza",
+  },
+};
