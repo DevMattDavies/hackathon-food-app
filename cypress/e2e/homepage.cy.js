cef0c41323606573e6fd827204419795739a7ed4
Emily <103897085+Emily9903@users.noreply.github.com>
2022-09-09 15:05:42 +0100

    Revert "cypress test pizza name display added"

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,16 +1,8 @@
-describe('Light/Dark Mode', () => {
+describe('Open Homepage', () => {
     it('passes', () => {
       cy.visit("http://localhost:3000");
       cy.contains('Light')
       cy.get('.react-switch-bg').click()
       cy.contains('Dark')
-    })
-  })
-
-  describe('Pizza Name Display', () => {
-    it('passes', () => {
-      cy.visit("http://localhost:3000");
-      cy.contains('Goat Cheese Pesto Pizza')
-      cy.contains('Rustic Grilled Peaches Pizza')
     })
   })
